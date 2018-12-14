a102ecc4e93db06156ff3859538c9014c5b7ec08
Francisco <franciscoigor@gmail.com>
2018-12-13 20:23:52 -0800

    updated path to serve JS

--- index.php
+++ index.php
@@ -1,3 +1,10 @@
 <?php
 
-print_r($_SERVER["PATH_INFO"]);
+$PATH = @$_SERVER["PATH_INFO"];
+
+if ($PATH=="/instagram.js"){
+    include("scripts/instagram/posts.php");
+    return;
+}
+
+header("HTTP 400 Bad Request");
